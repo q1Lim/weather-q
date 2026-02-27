68fe30d438baffe188e8e80188dd3ae9b33e75f4
q1Lim <cochlea00@gmail.com>
2026-02-27 22:43:33 +0900

    chore: constants 이동

---
+++ src/lib/weather.constants.ts
@@ -0,0 +1,7 @@
+export const WEATHER_API_BASE_URL = 'https://api.weatherapi.com/v1'
+export const WEATHER_API_ROUTES = {
+  current: '/current.json',
+  forecast: '/forecast.json',
+}
+export const DEFAULT_LANG = 'ko'
+export const DEFAULT_FORECAST_DAYS = 3
